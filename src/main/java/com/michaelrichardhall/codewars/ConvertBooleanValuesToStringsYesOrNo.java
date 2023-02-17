340635d5fa0ded883707cb401531d07808e53a30
gontofe <gontofe@hotmail.com>
2023-02-17 14:42:20 +0000

    adding files from 20230217

---
+++ src/main/java/com/michaelrichardhall/codewars/ConvertBooleanValuesToStringsYesOrNo.java
@@ -0,0 +1,10 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Convert boolean values to strings 'Yes' or 'No'.
+//        Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+public class ConvertBooleanValuesToStringsYesOrNo {
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+}
