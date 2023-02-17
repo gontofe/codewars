a5cf2c85a151596c47200c0615c9231c4072cb9c
gontofe <gontofe@hotmail.com>
2023-02-17 14:42:20 +0000

    adding files from 20230217

---
+++ src/main/java/com/michaelrichardhall/codewars/ConvertANumberToAString.java
@@ -0,0 +1,17 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Convert a Number to a String!
+//We need a function that can transform a number (integer) into a string.
+//
+//        What ways of achieving this do you know?
+//
+//        Examples (input --> output):
+//        123  --> "123"
+//        999  --> "999"
+//        -100 --> "-100"
+
+public class ConvertANumberToAString {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
