b2551546d713331ea31b387ddd2de6bd4619fec4
gontofe <gontofe@hotmail.com>
2023-02-19 17:20:23 +0000

    adding files from 20230219

---
+++ src/main/java/com/michaelrichardhall/codewars/TicTacToeChecker.java
@@ -0,0 +1,47 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Tic-Tac-Toe Checker
+//If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+//
+//Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+//
+//[[0, 0, 1],
+// [0, 1, 2],
+// [2, 1, 0]]
+//We want our function to return:
+//
+//-1 if the board is not yet finished AND no one has won yet (there are empty spots),
+//1 if "X" won,
+//2 if "O" won,
+//0 if it's a cat's game (i.e. a draw).
+//You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
+public class TicTacToeChecker {
+    public static int isSolved(int[][] board) {
+        int winner = 0;
+        int[][] vert = new int[3][];
+        int[][] diag = new int[2][];
+
+        vert[0] = new int[]{board[0][0], board[1][0], board[2][0]};
+        vert[1] = new int[]{board[0][1], board[1][1], board[2][1]};
+        vert[2] = new int[]{board[0][2], board[1][2], board[2][2]};
+
+        diag[0] = new int[]{board[0][0], board[1][1], board[2][2]};
+        diag[1] = new int[]{board[0][2], board[1][1], board[2][0]};
+
+        for (int[][] boards : new int[][][]{board, vert, diag}) {
+            for (int[] row : boards) {
+                for (int i : row) {
+                    if (i != 0) {
+                        if (row[0] == row[1] && row[1] == row[2]) {
+                            return row[0];
+                        }
+                    } else {
+                        winner = -1;
+                    }
+                }
+            }
+        }
+        return winner;
+    }
+}
