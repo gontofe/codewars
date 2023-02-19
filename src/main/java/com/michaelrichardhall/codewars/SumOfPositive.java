dda9853c301a31f278d536c5f5a9728c01221e5b
gontofe <gontofe@hotmail.com>
2023-02-19 17:20:23 +0000

    adding files from 20230219

---
+++ src/main/java/com/michaelrichardhall/codewars/SumOfPositive.java
@@ -0,0 +1,18 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+import java.util.Arrays;
+
+//Sum of positive
+//You get an array of numbers, return the sum of all of the positives ones.
+//
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//
+//Note: if there is nothing to sum, the sum is default to 0.
+
+public class SumOfPositive {
+  public static int sum(int[] arr){
+    return Arrays.stream(arr)
+      .filter(i -> Integer.signum(i)  > 0)
+      .sum();
+  }
+}
