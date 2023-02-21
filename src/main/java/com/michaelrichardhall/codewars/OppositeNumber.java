4db142539405bb6a07e6ea8444fdc41d74528c05
gontofe <gontofe@hotmail.com>
2023-02-21 17:05:43 +0000

    adding files from 20230221

---
+++ src/main/java/com/michaelrichardhall/codewars/OppositeNumber.java
@@ -0,0 +1,17 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Opposite number
+//Very simple, given an integer or a floating-point number, find its opposite.
+//
+//        Examples:
+//
+//        1: -1
+//        14: -14
+//        -34: 34
+
+public class OppositeNumber {
+    public static int opposite(int number)
+    {
+        return -number;
+    }
+}
