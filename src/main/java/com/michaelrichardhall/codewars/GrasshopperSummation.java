6e58ebb055104776d18e016a0de4f4c9fafdc0a9
gontofe <gontofe@hotmail.com>
2023-02-21 17:05:43 +0000

    refactor 1

--- src/main/java/com/michaelrichardhall/codewars/GrasshopperSummation.java
+++ src/main/java/com/michaelrichardhall/codewars/GrasshopperSummation.java
@@ -13,7 +13,7 @@ import java.util.stream.IntStream;
 public class GrasshopperSummation {
     public static int summation(int n) {
 
-        return IntStream.range(0, n + 1)
+        return IntStream.rangeClosed(1, n)
                 .reduce(Integer::sum)
                 .getAsInt();
     }
