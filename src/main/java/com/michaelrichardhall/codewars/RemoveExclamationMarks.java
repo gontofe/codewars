d3a5a123c879e154a019f218d7de8166ea0adf6d
gontofe <gontofe@hotmail.com>
2023-02-25 16:50:34 +0000

    adding files from 20230225

---
+++ src/main/java/com/michaelrichardhall/codewars/RemoveExclamationMarks.java
@@ -0,0 +1,10 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Remove exclamation marks
+//        Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+public class RemoveExclamationMarks {
+    static String removeExclamationMarks(String s) {
+        return s.replace("!","");
+    }
+}
