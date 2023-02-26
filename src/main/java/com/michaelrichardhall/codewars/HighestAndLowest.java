294f447f1c9ddf01b7689c73ea5db7b4ec1a21d2
gontofe <gontofe@hotmail.com>
2023-02-26 11:55:34 +0000

    adding files from 20230226

---
+++ src/main/java/com/michaelrichardhall/codewars/HighestAndLowest.java
@@ -0,0 +1,24 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+import java.util.Arrays;
+
+//Highest and Lowest
+//        In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+//
+//        Examples
+//        highAndLow("1 2 3 4 5")  // return "5 1"
+//        highAndLow("1 2 -3 4 5") // return "5 -3"
+//        highAndLow("1 9 3 4 -5") // return "9 -5"
+//        Notes
+//        All numbers are valid Int32, no need to validate them.
+//        There will always be at least one number in the input string.
+//        Output string must be two numbers separated by a single space, and highest number is first.
+
+public class HighestAndLowest {
+    public static String highAndLow(String numbers) {
+        var stats = Arrays.stream(numbers.split(" "))
+                .mapToInt(Integer::parseInt)
+                .summaryStatistics();
+        return stats.getMax() + " " + stats.getMin();
+    }
+}
