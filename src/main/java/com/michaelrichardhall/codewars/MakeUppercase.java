fcdc13e82eb09850ed48b4f0721d00fff5240e8b
gontofe <gontofe@hotmail.com>
2023-02-26 15:08:15 +0000

    adding files from 20230226

---
+++ src/main/java/com/michaelrichardhall/codewars/MakeUppercase.java
@@ -0,0 +1,10 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//MakeUpperCase
+//        Write a function which converts the input string to uppercase.
+
+public class MakeUppercase {
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
