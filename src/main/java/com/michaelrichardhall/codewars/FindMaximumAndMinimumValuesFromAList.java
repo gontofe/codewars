2a3c0e6038926eaa112fca5bfcaf4fb7bb7344a7
Mike <gontofe@hotmail.com>
2023-02-27 11:05:47 +0000

    adding files from 20230227

---
+++ src/main/java/com/michaelrichardhall/codewars/FindMaximumAndMinimumValuesFromAList.java
@@ -0,0 +1,28 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+import java.util.stream.IntStream;
+
+//Find Maximum and Minimum Values of a List
+//Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+//
+//        Examples (Input -> Output)
+//        * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+//        * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+//        * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+//        * [5]                             -> min = 5, max = 5
+//        Notes
+//        You may consider that there will not be any empty arrays/vectors.
+
+public class FindMaximumAndMinimumValuesFromAList {
+    public int min(int[] list) {
+        return IntStream.of(list)
+                .min()
+                .getAsInt();
+    }
+
+    public int max(int[] list) {
+        return IntStream.of(list)
+                .max()
+                .getAsInt();
+    }
+}
