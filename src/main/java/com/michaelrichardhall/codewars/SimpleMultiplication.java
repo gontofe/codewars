6e65d2895ccf7dabcdcbaa525f814358778d42d6
gontofe <gontofe@hotmail.com>
2023-03-05 15:24:03 +0000

    committing files from 20230305

---
+++ src/main/java/com/michaelrichardhall/codewars/SimpleMultiplication.java
@@ -0,0 +1,10 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Simple multiplication
+//        This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+public class SimpleMultiplication {
+    public static int simpleMultiplication(int n) {
+        return n % 2 == 0 ? n * 8 : n * 9;
+    }
+}
