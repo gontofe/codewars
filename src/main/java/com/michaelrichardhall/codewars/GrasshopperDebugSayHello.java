5bc42416708fd34968818b6ecc65f8050b354989
gontofe <gontofe@hotmail.com>
2023-03-08 17:21:45 +0000

    Grasshopper Debug sayHello Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/GrasshopperDebugSayHello.java
@@ -0,0 +1,15 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Grasshopper - Debug sayHello
+//The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+//
+//        Example output:
+//
+//        Hello, Mr. Spock
+
+public class GrasshopperDebugSayHello {
+    public static String sayHello(String name) {
+
+        return "Hello, " + name;
+    }
+}
