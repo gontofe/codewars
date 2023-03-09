5b1568080e6485d02503959e5fd09b802e06e4aa
gontofe <gontofe@hotmail.com>
2023-03-09 17:33:48 +0000

    Switch it Up! Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/SwitchItUp.java
@@ -0,0 +1,37 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Switch it Up!
+//When provided with a number between 0-9, return it in words.
+//
+//        Input :: 1
+//
+//        Output :: "One".
+//
+//        If your language supports it, try using a switch statement.
+
+public class SwitchItUp {
+    public static String switchItUp(int number) {
+        switch(number) {
+            case 0:
+                return "Zero";
+            case 1:
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+            case 7:
+                return "Seven";
+            case 8:
+                return "Eight";
+            default:
+                return "Nine";
+        }
+    }
+}
