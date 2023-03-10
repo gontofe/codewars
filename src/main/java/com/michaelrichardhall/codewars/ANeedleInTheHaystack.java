3c0ea85e99eeca3dd34b8a349cfd973a210d006d
gontofe <gontofe@hotmail.com>
2023-03-10 16:44:34 +0000

    A Needle in the Haystack Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/ANeedleInTheHaystack.java
@@ -0,0 +1,26 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//A Needle in the Haystack
+//Can you find the needle in the haystack?
+//
+//        Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//
+//        After your function finds the needle it should return a message (as a string) that says:
+//
+//        "found the needle at position " plus the index it found the needle, so:
+//
+//        Example(Input --> Output)
+//
+//        ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+//        Note: In COBOL, it should return "found the needle at position 6"
+
+public class ANeedleInTheHaystack {
+    public static String findNeedle(Object[] haystack) {
+        int position = 0;
+        for ( int i = 0; i < haystack.length; i++ )
+            if ( "needle".equals( haystack[i] ) )
+                position = i;
+
+        return "found the needle at position " + position;
+    }
+}
