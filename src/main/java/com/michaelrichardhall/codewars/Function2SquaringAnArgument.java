6053c943731dee5367fa5f08cb91efd7bb81609f
gontofe <gontofe@hotmail.com>
2023-03-23 16:43:44 +0000

    Function 2 - Squaring an argument Kata

--- src/main/java/com/michaelrichardhall/codewars/Function2SquaringAnArgument.java
+++ src/main/java/com/michaelrichardhall/codewars/Function2SquaringAnArgument.java
@@ -1,4 +1,10 @@
 package src.main.java.com.michaelrichardhall.codewars;
 
+//Function 2 - squaring an argument
+//        Now you have to write a function that takes an argument and returns the square of it.
+
 public class Function2SquaringAnArgument {
+    public static int square(int n) {
+        return n * n;
+    }
 }
