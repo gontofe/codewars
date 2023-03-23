a06b9da2517f23c428e419be016bd5415171f513
gontofe <gontofe@hotmail.com>
2023-03-23 16:56:43 +0000

    get character from ASCII value Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/GetCharacterFromASCIIValue.java
@@ -0,0 +1,18 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//get character from ASCII Value
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+//
+//Example:
+//
+//get_char(65)
+//should return:
+//
+//'A'
+//For ASCII table, you can refer to http://www.asciitable.com/
+
+public class GetCharacterFromASCIIValue {
+    public static char getChar(int c) {
+        return (char)c;
+    }
+}
