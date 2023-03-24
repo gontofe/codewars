cb50cae23992b7b8128cd51aa19f595f66ed42b0
gontofe <gontofe@hotmail.com>
2023-03-24 16:16:27 +0000

    Bin to Decimal Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/BinToDecimal.java
@@ -0,0 +1,10 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+//Bin to Decimal
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+public class BinToDecimal {
+    public static int binToDecimal(String inp){
+        return Integer.parseInt(inp, 2);
+    }
+}
