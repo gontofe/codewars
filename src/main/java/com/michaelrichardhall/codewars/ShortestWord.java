86f538fa2d1d0c7aabe710e40a8998362354a4de
mike <michaelrichardhall@gmail.com>
2023-03-30 16:02:05 +0100

    Refactor Shortest Word Kata

--- src/main/java/com/michaelrichardhall/codewars/ShortestWord.java
+++ src/main/java/com/michaelrichardhall/codewars/ShortestWord.java
@@ -1,7 +1,6 @@
 package src.main.java.com.michaelrichardhall.codewars;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 //Shortest Word
 //Simple, given a string of words, return the length of the shortest word(s).
@@ -11,8 +10,8 @@ import java.util.Comparator;
 public class ShortestWord {
     public static int findShort(String s) {
         return Arrays.stream(s.split(" "))
-                .min(Comparator.comparing(String::length))
-                .map(String::length)
-                .orElse(0);
+                .mapToInt(String::length)
+                .min()
+                .getAsInt();
     }
 }
