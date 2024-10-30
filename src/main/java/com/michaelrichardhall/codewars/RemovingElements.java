70878db2034a86368ef64e061c36a90caf2be672
Mike Hall <mike.2.hall@morrisonsplc.co.uk>
2024-10-30 15:00:53 +0000

    Removing Elements Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/RemovingElements.java
@@ -0,0 +1,13 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class RemovingElements {
+    public static Object[] removeEveryOther(Object[] arr) {
+        return IntStream.range(0, arr.length)
+                .filter(i -> i % 2 != 0)
+                .mapToObj(i -> arr[i])
+                .toArray();
+    }
+}
