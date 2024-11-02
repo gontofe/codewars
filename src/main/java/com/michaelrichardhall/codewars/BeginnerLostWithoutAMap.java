cf95a7e23c4031a2f638f2c511354abc4e65f479
Mike <michaelrichardhall@gmail.com>
2024-11-02 08:37:41 +0000

    Beginner - Lost Without a Map Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/BeginnerLostWithoutAMap.java
@@ -0,0 +1,17 @@
+package src.main.java.com.michaelrichardhall.codewars;
+
+import java.util.Arrays;
+/*
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr)
+                .map(i -> i * 2)
+                .toArray();
+    }
+ */
+public class BeginnerLostWithoutAMap {
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr)
+                .map(i -> i * 2)
+                .toArray();
+    }
+}
