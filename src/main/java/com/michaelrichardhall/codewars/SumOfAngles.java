1633db7cf0a9ae45b9cac29c90e4e1b72b68e370
Mike Hall <mike.2.hall@morrisonsplc.co.uk>
2024-12-04 15:01:22 +0000

    Sum of Angles Kata

---
+++ src/main/java/com/michaelrichardhall/codewars/SumOfAngles.java
@@ -0,0 +1,9 @@
+package src.main.java.com.michaelrichardhall.codewars;
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+ */
+public class SumOfAngles {
+    public static int sumOfAngles(int n) {
+        return (n - 2) * 180;
+    }
+}
